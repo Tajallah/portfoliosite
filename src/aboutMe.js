a6f9b7302b67dfd9ecdaf4ce7efcc1b82f3d0fa2
Ana@tsukuru <tajallah.sims@gmail.com>
2021-04-03 16:37:22 -0700

    wording

--- src/aboutMe.js
+++ src/aboutMe.js
@@ -17,16 +17,16 @@ class AboutMe extends Component {
           <div className="Content-Box">
             <div className="Text-Header">Me,</div>
             <br></br>
-            <div className="Text-Body">I've been writing code as a hobby since 
-            around 17 and I love cats. I'm a vegetarian from the LA area area who hates talking about 
-            themself and loves learning new things. Also I'm a sagittarius.<br></br><br></br>
-            Regarding code, I'm a full stack developer with expertise in 
-            backend technologies and automation. My professional background 
+            <div className="Text-Body">I've been writing code as a hobby since
+            around 17 and I love cats. I'm a vegetarian from the LA area area who hates talking about
+            themself and loves learning new things.<br></br><br></br>
+            Regarding code, I'm a full stack developer with expertise in
+            backend technologies and automation. My professional background
             is mostly related to information technology so I know my way
-            around a Linux box and am no strnager to automating "the boring 
-            stuff". This website was built with React and is hosted on a cloud provider in a Docker container.<br></br><br></br>I have a passion for data science and 
-            analytics and tend to spend more time than I should reading 
-            ml-related papers. One day I'll get around to using 
+            around a Linux box and am no strnager to automating "the boring
+            stuff". This website was built with React and is hosted on a cloud provider in a Docker container.<br></br><br></br>I have a passion for data science and
+            analytics and tend to spend more time than I should reading
+            ml-related papers. One day I'll get around to using
             Kaggle as more than just place to bum datasets.<br></br>
             <br></br><Link to="/" className="Text-Button">Take me back</Link>
             <Link to="/projects" className="Text-Button">Show me some projects</Link>
