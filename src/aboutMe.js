515bc91abe87786e8f5c71924ae8028b6ec8110d
Ana <tajallah.sims@gmail.com>
2021-05-07 13:58:04 -0700

    fixing typos

--- src/aboutMe.js
+++ src/aboutMe.js
@@ -18,12 +18,12 @@ class AboutMe extends Component {
             <div className="Text-Header">Me,</div>
             <br></br>
             <div className="Text-Body">I've been writing code as a hobby since
-            around 17 and I love cats. I'm a vegetarian from the LA area area who hates talking about
+            around 17 and I love cats. I'm a vegetarian from the LA area who hates talking about
             themself and loves learning new things.<br></br><br></br>
             Regarding code, I'm a full stack developer with expertise in
             backend technologies and automation. My professional background
             is mostly related to information technology so I know my way
-            around a Linux box and am no strnager to automating "the boring
+            around a Linux box and am no stranger to automating "the boring
             stuff". This website was built with React and is hosted on a cloud provider in a Docker container.<br></br><br></br>I have a passion for data science and
             analytics and tend to spend more time than I should reading
             ml-related papers. One day I'll get around to using
